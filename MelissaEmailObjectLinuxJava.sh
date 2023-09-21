3cf9e237af4788156f7357386b8b371e9f92d226
Melissa <>
2023-09-21 09:17:21 -0700

    Tested successfully using Melissa data files for 2023.09

--- MelissaEmailObjectLinuxJava.sh
+++ MelissaEmailObjectLinuxJava.sh
@@ -44,7 +44,7 @@ done
 
 
 # ######################### Config ###########################
-RELEASE_VERSION='2023.08'
+RELEASE_VERSION='2023.09'
 ProductName="DQ_EMAIL_DATA"
 
 # Uses the location of the .sh file 
